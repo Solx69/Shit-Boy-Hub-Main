30acc8acaf1beaf269fc5ab40e4c0859f9c81600
Solx69 <72536637+Solx69@users.noreply.github.com>
2020-12-18 06:13:35 -0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Changed the loadstring. Join the discord for updated script: https://discord.gg/K5JHaxTK")
+game.Players.LocalPlayer:Kick("Changed the loadstring. Join the discord for updated script: https://discord.gg/c8F5XkyP3m")
